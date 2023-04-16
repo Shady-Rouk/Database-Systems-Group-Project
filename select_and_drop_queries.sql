9eb316d4018dd1d70a744326effcba8ba01c6099
Farouk Balogun <73665722+Shady-Rouk@users.noreply.github.com>
2023-04-16 15:04:38 -0400

    sql queries to show and drop the tables

---
+++ select_and_drop_queries.sql
@@ -0,0 +1,33 @@
+DROP TABLE homework;
+
+DROP TABLE participation;
+
+DROP TABLE project;
+
+DROP TABLE test;
+
+DROP TABLE categories;
+
+DROP TABLE student;
+
+DROP TABLE course;
+
+DROP TABLE student_total_grade;
+
+
+
+SELECT * FROM course;
+
+SELECT * FROM categories;
+
+SELECT * FROM homework ORDER BY crn;
+
+SELECT * FROM participation ORDER BY crn;
+
+SELECT * FROM project ORDER BY crn;
+
+SELECT * FROM test ORDER BY crn;
+
+SELECT * FROM student ORDER BY crn;
+
+SELECT * FROM student_total_grade;
